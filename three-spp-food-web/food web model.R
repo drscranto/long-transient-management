f4b0bd950c22a6313bdadfee020245a3fdbbc506
Katherine Scranton <scranton.kt@gmail.com>
2018-09-27 15:57:38 -0400

    Setting up new model

---
+++ three-spp-food-web/food web model.R
@@ -0,0 +1,8 @@
+## Case Study for management paper
+## Katie Scranton
+## scranton.kt@gmail.com
+## 9/27/2018
+
+## 3 species food web
+
+## When computing with this model, use ln N_i, not N_i as your variables – it is much much more efficient, and if you want to add noise has the advantage of not have to worry so much about crossing zero
